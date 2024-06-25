6016b300063407fb950871bf96604ed29d1835ce
Simona <trajkovskasimona10@gmail.com>
2024-06-25 16:18:02 +0200

    finishing the frontend of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
       extend: {
           colors: {
               "black": "#060606"
+          },
+          fontFamily:{
+            "hanken-gtotesk":['Hanken Grotesk', 'sans-serif']
+          },
+          fontSize:{
+            "2xs":".625rem" //10px
           }
       },
   },
